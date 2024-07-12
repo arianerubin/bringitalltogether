54f86efcea4b43a75809214b66b8a406292bfc1b
Maryfidlay <mariefidlay@gmail.com>
2024-07-12 09:34:53 -0700

    working

--- src/queries/userQueries.js
+++ src/queries/userQueries.js
@@ -1,5 +1,6 @@
 const { bcrypt, prisma, jwt } = require("../shared/shared");
 const registerQuery = async ({ firstName, lastName, email, password }) => {
+  console.log(password);
   const hashPassword = await bcrypt.hash(password, 10);
   const registerUser = await prisma.user.create({
     data: {
@@ -18,7 +19,12 @@ const registerQuery = async ({ firstName, lastName, email, password }) => {
       expiresIn: "1h",
     }
   );
-  return token;
+  return {
+    token: token,
+    firstName: registerUser.firstName,
+    lastName: registerUser.lastName,
+    email: registerUser.email,
+  };
 };
 
 module.exports = {
